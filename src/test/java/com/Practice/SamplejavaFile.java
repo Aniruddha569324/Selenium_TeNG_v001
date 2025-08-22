963cdf81c0f8a4d457aaae96c1585df494484937
Aniruddha Shinde <aniruddha.shinde50@gmail.com>
2025-08-22 20:09:05 +0530

    corrected sample file

--- src/test/java/com/Practice/SamplejavaFile.java
+++ src/test/java/com/Practice/SamplejavaFile.java
@@ -1,6 +1,8 @@
+package com.Practice;
+
 class SamplejavaFile
   {
-    public static void main(String args[]
+    public static void main(String args[])
                             {
       
       System.out.println("hello");
